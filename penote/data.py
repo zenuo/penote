83be72d3928aae2e79fb6e9fac667d085266cbcf
yuanzhen <zenuor@gmail.com>
2018-04-10 17:23:05 +0800

    修改会话制造器

--- penote/data.py
+++ penote/data.py
@@ -9,4 +9,4 @@ LOGGER = logging.getLogger(__name__)
 __ENGINE = create_engine('sqlite:///penote.db', echo=True)
 # 若不存在表，则创建表
 __BASE.metadata.create_all(__ENGINE)
-SESSION_MAKER = sessionmaker(bind=__ENGINE)
+SESSION_MAKER = sessionmaker(bind=__ENGINE, expire_on_commit=False)
