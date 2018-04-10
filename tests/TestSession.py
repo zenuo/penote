470e118f9e19aa9a34af8bcca4e127e9788df8f1
yuanzhen <zenuor@gmail.com>
2018-04-10 23:24:14 +0800

    添加会话测试

--- tests/TestSession.py
+++ tests/TestSession.py
@@ -8,7 +8,7 @@ class SessionTestCase(unittest.TestCase):
         user_name = 'hello'
         self.assertFalse(session.exists_by_user_name(user_name))
 
-    def test_invalidate_by_user_id(self):
-        user_id = '3ccb0d3e-832d-418e-8aae-de75f2d2f208'
-        session.invalidate_by_user_id(user_id)
-        self.assertFalse(session.exists_by_user_name('hello'))
+    def test_invalidate_by_user_name(self):
+        user_name = 'hello'
+        session.invalidate_by_user_name(user_name)
+        self.assertFalse(session.exists_by_user_name(user_name))
