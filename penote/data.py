fd270686cd350ed0f7968b5febb99df40264ceb6
yuanzhen <zenuor@gmail.com>
2018-04-10 11:13:13 +0800

    修改成员名称

--- penote/data.py
+++ penote/data.py
@@ -3,10 +3,10 @@ import logging
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from penote.entity import BASE
+from penote.models import BASE as __BASE
 
 LOGGER = logging.getLogger(__name__)
 __ENGINE = create_engine('sqlite:///penote.db', echo=True)
 # 若不存在表，则创建表
-BASE.metadata.create_all(__ENGINE)
+__BASE.metadata.create_all(__ENGINE)
 SESSION_MAKER = sessionmaker(bind=__ENGINE)
