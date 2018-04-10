5e45cbe02135346a5ff8c0dbb261c819b4a5697c
yuanzhen <zenuor@gmail.com>
2018-04-10 23:25:50 +0800

    添加部分用户服务

--- penote/service/user.py
+++ penote/service/user.py
@@ -3,6 +3,7 @@ import uuid
 from hashlib import sha3_512
 
 from flask_restful import abort
+from sqlalchemy import func
 
 from ..data import SESSION_MAKER
 from ..models import User
@@ -14,7 +15,7 @@ LOGGER = logging.getLogger(__name__)
 def create(json):
     if {'name', 'email', 'bio', 'password'} != set(json.keys()):
         abort(400, error='信息不完全')
-    exists_same_name = exists_name(json['name'])
+    exists_same_name = exists_by_name(json['name'])
     if exists_same_name:
         abort(400, error='存在同名用户')
     else:
@@ -37,12 +38,74 @@ def create(json):
             sess.close()
 
 
-def exists_name(name):
+def exists_by_name(name):
     sess = SESSION_MAKER()
     try:
-        return sess.query(User).filter(User.name == name).count() != 0
+        return sess.query(func.count(User.id)).filter_by(name=name, is_deleted=0).limit(1).scalar() != 0
     except Exception as ex:
-        LOGGER.error("查询同名用户异常", ex)
+        LOGGER.error('查询是否存在同名用户异常', ex)
         raise ex
+    finally:
+        sess.close()
+
+
+def exists_by_id(id, is_deleted):
+    sess = SESSION_MAKER()
+    try:
+        return sess.query(func.count(User.id)).filter_by(id=id, is_deleted=is_deleted).limit(1).scalar() != 0
+    except Exception as ex:
+        LOGGER.error('查询是否存在指定ID用户异常', ex)
+        raise ex
+    finally:
+        sess.close()
+
+
+def invalidate(id, session):
+    # if not is_valid(session):
+    # abort(403, error='无权限，请登录')
+    if exists_by_id(id, 0):
+        sess = SESSION_MAKER()
+        try:
+            sess.query(User).filter_by(id=id, is_deleted=0).update({'is_deleted': 1})
+            sess.commit()
+        except Exception as ex:
+            LOGGER.error('禁用用户异常', ex)
+            raise ex
+        finally:
+            sess.close()
+    else:
+        abort(404, error='不存在指定用户')
+
+
+def validate(id, session):
+    # if not is_valid(session):
+    # abort(403, error='无权限，请登录')
+    if exists_by_id(id, 1):
+        sess = SESSION_MAKER()
+        try:
+            sess.query(User).filter_by(id=id, is_deleted=1).update({'is_deleted': 0})
+            sess.commit()
+        except Exception as ex:
+            LOGGER.error('禁用用户异常', ex)
+            raise ex
+        finally:
+            sess.close()
+    else:
+        abort(404, error='不存在指定的被禁用的用户')
+
+
+def check_password(user_name, password):
+    sess = SESSION_MAKER()
+    try:
+        user_id = sess.query(User.id). \
+            filter_by(name=user_name, password_hash=sha3_512(password.encode('utf-8')).hexdigest(), is_deleted=0). \
+            limit(1). \
+            scalar()
+        if user_id:
+            return True, user_id
+        else:
+            return False, None
+    except Exception as ex:
+        LOGGER.error('检查密码异常', ex)
     finally:
         sess.close()
