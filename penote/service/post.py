2e53fbe025e3b00aeb42a0ac0767051638d0909e
zenuo <zenuor@gmail.com>
2018-05-04 00:02:56 +0800

    迁移文件上传逻辑至upload

--- penote/service/post.py
+++ penote/service/post.py
@@ -1,26 +1,24 @@
+""" 文章逻辑 """
 import logging
 import os
+import uuid
+
 from werkzeug.utils import secure_filename
 
-from penote import config
-from penote.data import SESSION_MAKER
-from penote.models import Post
+from ..config import get_config
+from ..data import SESSION_MAKER
+from ..models import Post
 
 LOGGER = logging.getLogger(__name__)
-CONFIG = config.get()
-
-
-def upload(file):
-    file_path = os.path.join(CONFIG['source_image_path'], secure_filename(file.filename))
-    file.save(file_path)
-    LOGGER.info('保存上传文件%s' % file_path)
-    return 'true'
+CONFIG = get_config()
 
 
 def get(id, user=None):
     sess = SESSION_MAKER()
     try:
-        return sess.query(Post).filter_by(id=id, is_deleted=0).all()
+        return sess.query(Post).\
+            filter_by(id=id, is_deleted=0).\
+            all()
     except Exception as ex:
         LOGGER.error('查询文章失败', ex)
     finally:
---
+++ penote/service/upload.py
@@ -0,0 +1,41 @@
+import logging
+import os
+import uuid
+import json
+
+from werkzeug.utils import secure_filename
+
+from ..config import get_config
+
+LOGGER = logging.getLogger(__name__)
+# 被支持的图像文件后缀名
+__SUPPORTED_IMAGE_FILE_EXTENSION = {'jpg', 'jpeg', 'bmp'}
+
+
+def save(file):
+    """ 保存上传的文件,以随机UUID保存,并返回UUID """
+    # 文件路径
+    extension = __get_support_image_file_extension(file.filename)
+    if not extension:
+        return json.dumps({'key': None})
+    else:
+        file_key = secure_filename(str(uuid.uuid4()) + '.' + extension)
+        file_path = os.path.join(
+            get_config().get('source_image_path'),
+            file_key
+        )
+        file.save(file_path)
+        file.close()
+        LOGGER.info('保存上传文件%s', file_path)
+        return json.dumps({'key': file_key})
+
+
+def __get_support_image_file_extension(filename):
+    """ 获取被支持的图像文件后缀名 """
+    tokens = filename.split('.')
+    if len(tokens) < 2:
+        return None
+    elif tokens[-1].lower() in __SUPPORTED_IMAGE_FILE_EXTENSION:
+        return tokens[-1].lower()
+    else:
+        return None
